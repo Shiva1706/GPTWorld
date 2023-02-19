9ca3c430ae65c5b2f534d57a95059ef8b2f5b21f
Shiva1706 <shivansh.singh2019@vitbhopal.ac.in>
2023-02-19 18:04:55 +0530

    server url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   },[posts])
 
   const fetchBotResponse = async() =>{
-    const { data } = await axios.post("http://localhost:4000", {input},
+    const { data } = await axios.post("https://gptworld.onrender.com", {input},
       {headers: {"Content-Type": "application/json"}}
     );
 
